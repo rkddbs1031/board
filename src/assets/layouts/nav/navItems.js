8ddf387a740bedac41497c302f89ad1d49ae1607
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-04-15 18:00:46 +0900

    [ING] 작업 세팅

--- src/assets/layouts/nav/navItems.js
+++ src/assets/layouts/nav/navItems.js
@@ -3,12 +3,4 @@ export default [
     title: "홈",
     to: "/",
   },
-  {
-    title: "무드선택",
-    to: "/mood",
-  },
-  {
-    title: "마이플레이",
-    to: "/myPlay",
-  },
 ];
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,13 @@ import Router from "vue-router";
 Vue.use(Router);
 
 const Layout = () => import("@/assets/layouts/Layout.vue");
-
+const BlankLayout = () => import("@/assets/layouts/BlankLayout.vue");
 const Home = () => import("@/views/Home.vue");
-const Mood = () => import("@/components/mood/Mood.vue");
-const Myplay = () => import("@/components/myplay/Myplay.vue");
+// const Mood = () => import("@/components/mood/Mood.vue");
+// const Myplay = () => import("@/components/myplay/Myplay.vue");
+
+const Login = () => import("@/components/auth/Login.vue");
+const register = () => import("@/components/auth/Register.vue");
 
 const router = new Router({
   mode: "history",
@@ -15,22 +18,27 @@ const router = new Router({
   routes: [
     {
       path: "/",
+      redirect: "/auth/login",
+      component: BlankLayout,
+      children: [
+        {
+          path: '/auth/login',
+          component: Login,
+        },
+        {
+          path: '/auth/register',
+          component: register,
+        },
+      ],
+    },
+    {
+      path: "/board",
       component: Layout,
       children: [
         {
-          path: "",
-          name: "Home",
+          path: "/board",
+          name: "Board",
           component: Home,
-        },
-        {
-          path: "/mood",
-          name: "Mood",
-          component: Mood,
-        },
-        {
-          path: "/myplay",
-          name: "Myplay",
-          component: Myplay,
         },
       ],
     },
