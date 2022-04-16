978adcc3f0cc99809b6fe301a4bf3f64ec5e7871
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-04-16 21:13:40 +0900

    [ING] 게시판 리스트 작업중

--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>boardLists</div>
+  <v-container fluid class="down-top-padding">
+    <v-col>
+      <v-col class="d-flex justify-space-between align-center">
+        <h1>게시판</h1>
+        <v-btn @click="boardNew" color="primary" dark>글쓰기</v-btn>
+      </v-col>
+      <v-col>
+        <v-data-table
+          :headers="list.headers"
+          :items="list.desserts"
+          :items-per-page="10"
+          class="elevation-1"
+          @click:row="boardView"
+        ></v-data-table>
+      </v-col>
+    </v-col>
+  </v-container>
 </template>
+<script>
+export default {
+  name: "Board",
+  data: () => ({
+    list: {
+      headers: [],
+      desserts: [],
+    },
+  }),
+  methods: {
+    boardNew() {
+      console.log('boardNew');
+    },
+    boardView() {
+      console.log('boardView');
+    },
+  },
+};
+</script>
+<style scoped></style>
