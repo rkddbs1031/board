d5047aabd32df04a2a9acdd2814ad1c608394ad8
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-04-16 20:55:24 +0900

    [ADD] 로그인, 회원가입 페이지

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -16,46 +16,34 @@
           action="/"
           class="mt-4"
         >
-        <v-col class="py-0">
-          <v-text-field
-            v-model="username"
-            :rules="usernameRules"
-            label="Id"
-            required
-            outlined
-          />
-        </v-col>
-        <v-col class="py-0">
-          <v-text-field
-            v-model="password"
-            :rules="passwordRules"
-            label="Password"
-            required
-            outlined
-            :type="show1 ? 'text' : 'password'"
-          />
-        </v-col>
-          
-          <v-row>
-            <v-col>
-              <v-btn
-                :disabled="!valid"
-                block
-                class="mr-4"
-                submit
-                @click="boardLogin"
-                >로그인</v-btn
-              >
-            </v-col>
-            <v-col>
-              <v-btn
-                block
-                class="mr-4"
-                @click="boardRegister"
-                >회원가입</v-btn
-              >
-            </v-col>
-          </v-row>
+          <v-col class="py-0">
+            <v-text-field
+              v-model="username"
+              :rules="usernameRules"
+              label="Id"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+              outlined
+              :type="show1 ? 'text' : 'password'"
+            />
+          </v-col>
+          <v-col>
+            <v-btn
+              :disabled="!valid"
+              block
+              color="primary"
+              submit
+              @click="login"
+              >로그인</v-btn>
+          </v-col>
         </v-form>
       </v-col>
     </v-card>
@@ -63,50 +51,50 @@
 </template>
 
 <script>
-  export default {
-    name: "BoardLogin",
-    data: () => ({
-      valid: true,
-      username: "",
-      usernameRules: [
-        (v) => !!v || "필수 항목입니다.",
-      ],
-      show1: false,
-      password: "",
-      passwordRules: [
-        (v) => !!v || "Password is required",
-      ],
-    }),
-    methods: {
-      boardLogin() {
-        this.$refs.form.validate();
-        if (!this.username) {
-          alert("아이디를 입력해주세요.");
-        } else if (!this.password) {
-          alert("비밀번호를 입력해주세요.");
-        } else {
-          const authData = {
-            username: this.username,
-            password: this.password,
-          };
-          // // 동일한 로직을 vuex action을 통해 구현
-          // this.$store.dispatch("LOGIN", authData);
-        }
-      },
-      boardRegister() {
-        // this.$router.push({ path: "/board/register" });
-      },
+import axios from "axios";
+
+export default {
+  name: "BoardLogin",
+  data: () => ({
+    valid: true,
+    username: "",
+    usernameRules: [
+      (v) => !!v || "필수 항목입니다.",
+    ],
+    show1: false,
+    password: "",
+    passwordRules: [
+      (v) => !!v || "필수 항목입니다.ㄴㄴㄴㄴ",
+    ],
+  }),
+  methods: {
+    async login() {
+      this.$refs.form.validate();
+      const user = {
+        username: this.username,
+        password: this.password,
+      };
+      if (this.$refs.form.validate()) {
+        const API = `${process.env.VUE_APP_API_URL}/public/auth/login`;
+        await axios.post(API, user)
+          .then(() => this.$router.push('/board/lists'))
+          .catch((err) => {
+            console.log(err.response.status);
+            alert("아이디 또는 비밀번호가 맞지 않습니다.")
+          });
+      }
     },
-  };
-  </script>
-  <style scoped>
-    .main {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-    }
-    .main .v-card {
-      margin: 0 auto !important;
-    }
-  </style>
+  },
+};
+</script>
+<style scoped>
+  .main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  .main .v-card {
+    margin: 0 auto !important;
+  }
+</style>
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,3 +1,135 @@
 <template>
-  <div>Register.vue</div>
+  <v-col class="d-flex main" >
+    <v-card width="800">
+      <v-col>
+        <v-col>
+          <h2 class="font-weight-bold"> Sign Up </h2>
+          <h5 class="subtitle-1 d-flex justify-space-between font-weight-normal">
+            <span>회원이신가요?</span>
+            <span><a href="/auth/login" class>로그인하러 가기</a> </span>
+          </h5>
+        </v-col>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          class="mt-4"
+        >
+          <v-col class="py-0">
+            <v-text-field
+              v-model="userName"
+              :rules="Rules"
+              label="아이디"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field
+              v-model="fName"
+              :rules="Rules"
+              label="성"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field
+              v-model="lName"
+              :rules="Rules"
+              label="이름"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field 
+              v-model="pw"
+              :counter="10"
+              :rules="Rules"
+              label="비밀번호"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show1 ? 'text' : 'password'"
+              @click:append="show1 = !show1"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col class="py-0">
+            <v-text-field 
+              v-model="email"
+              :rules="emailRules"
+              label="이메일"
+              required
+              outlined
+            />
+          </v-col>
+          <v-col>
+            <v-btn
+              :disabled="!valid"
+              block
+              color="primary"
+              submit
+              @click="register"
+              >회원가입</v-btn>
+          </v-col>
+        </v-form>
+      </v-col>
+    </v-card>
+  </v-col>
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "Register",
+  data: () => ({
+    valid: true,
+    userName: '',
+    fName: '',
+    lName: '',
+    show1: false,
+    pw: '',
+    Rules: [(v) => !!v || "필수 항목입니다."],
+    email: '',
+    emailRules: [(v) => !!v || '필수 항목입니다.', (v) => /.+@.+\..+/.test(v) || '유효한 이메일을 작성해 주세요.'],
+  }),
+  methods: {
+    async register() {
+      this.$refs.form.validate();
+      
+      const userData = {
+        uername: this.userName,
+        password: this.pw,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+      };
+      
+      if (this.$refs.form.validate()) {
+        const API = `${process.env.VUE_APP_API_URL}/public/auth/register`;
+        await axios.post(API, userData)
+          .then(() => {
+            alert("회원가입이 완료되었습니다.");
+            this.$router.push('/auth/login');
+          })
+          .catch((err) => {
+            console.log(err.response.status);
+            alert("다시 시도 해주세요.");
+          });
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+    .main {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
+    .main .v-card {
+      margin: 0 auto !important;
+    }
+  </style>
