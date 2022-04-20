03d5798b7d23e24fc219e241f7ee2d81bccf0021
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-04-20 16:42:27 +0900

    [ADD] 게시판 리스트

--- src/views/board/Board.vue
+++ src/views/board/Board.vue
@@ -7,37 +7,64 @@
       </v-col>
       <v-col>
         <v-data-table
-          :headers="list.headers"
-          :items="list.desserts"
+          :headers="headers"
+          :items="items"
           :items-per-page="10"
           class="elevation-1"
           @click:row="boardView"
-        ></v-data-table>
+        >
+        <template v-slot:[`item.createdAt`]="{ item }">
+          <td>{{ new Date(item.createdAt).toLocaleString() }}</td>
+        </template>
+      </v-data-table>
       </v-col>
+      <!-- // table -->
     </v-col>
   </v-container>
 </template>
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: "Board",
   data: () => ({
-    list: {
-      headers: [],
-      desserts: [],
-    },
+    headers: [
+      { 
+        text: 'no',
+        value: "id",
+        sortable: false,
+        width: '10%',
+      },
+      {
+        text: '제목',
+        value: 'title',
+        sortable: false,
+      },
+      {
+        text: '작성일시',
+        value: 'createdAt',
+        sortable: false,
+        width: '25%',
+      },
+    ],
   }),
   computed: {
     ...mapState( 'user', ['token']),
+    ...mapState( 'list', [ 'items' ]),
   },
   created() {
     if (!this.token) {
       alert('로그인 정보가 없습니다. 로그인해주세요!');
       this.$router.push('/auth/login');
+    } else {
+      this.list();
     }
   },
   methods: {
+    ...mapActions( 'list', ['getBoard']),
+    list() {
+      this.getBoard({ token: this.token });
+    },
     boardNew() {
       console.log('boardNew');
     },
