0c39a3bda649551199840594b75a690b65648539
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-04-26 16:58:26 +0900

    [MOD] 게시판 상세 페이지 수정

--- src/views/board/BoardDetail.vue
+++ src/views/board/BoardDetail.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container>
+    <div class="delBtn text-right mt-10 mb-4">
+      <v-btn color="primary" class="font-weight-bold body-1"  @click="goBack">목록보기</v-btn>
+    </div>
     <v-card class="boardTable">
       <table class="table">
         <colgroup>
           <col style="width: 20%" />
-          <col style="width: 80%" />
+          <col style="width: 90%" />
         </colgroup>
         <tbody>
-          <tr class="tr border-bottom">
+          <tr class="tr">
             <th class="pa-5 border-right">제목</th>
             <td class="px-5">{{ boardInfo.title }}</td>
             <v-divider></v-divider>
@@ -21,14 +24,14 @@
         </tbody>
       </table>
     </v-card>
-    <v-col class="text-right">
-      <v-btn color="primary" class="font-weight-bold body-1 mr-2" @click="modify">수정하기</v-btn>
-      <v-btn color="primary" class="font-weight-bold body-1" outlined @click="goBack">뒤로가기</v-btn>
+    <v-col class="text-right px-0">
+      <v-btn color="error" class="font-weight-bold body-1 mr-3" outlined @click="del">삭제하기</v-btn>
+      <v-btn color="primary" class="font-weight-bold body-1" @click="modify">수정하기</v-btn>
     </v-col>
   </v-container>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions, mapMutations } from 'vuex';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { Viewer } from '@toast-ui/vue-editor';
 // import CommentList from "./comment/CommentList";
@@ -52,13 +55,24 @@ export default {
   },
   created() {
     this.getBoardDetail({ token: this.token, id: this.$route.params.id });
+
+    this.SET_BREADCRUMBS({
+      title: '게시판',
+      text: [
+        { text: '게시판', disabled: false, href: '/board/list' },
+        { text: '상세보기', disabled: true },
+        { text: this.boardInfo.title, disabled: true },
+      ],
+    });
   },
   methods: {
+    ...mapMutations( 'layout', ['SET_BREADCRUMBS']),
     ...mapActions('board', ['getBoardDetail']),
     modify() {},
     goBack() {
       this.$router.go(-1);
     },
+    del() {},
   },
 };
 </script>
@@ -69,6 +83,12 @@ export default {
     border-top: 3px solid #222;
     border-bottom: 2px solid #222;
     border-collapse: collapse;
+    .tr:nth-child(1) {
+      border-bottom: 1px solid #222;
+    }
+    tr th {
+      border-right: 1px solid rgba(34, 34, 34, 0.082);
+    }
   }
   tbody {
     border-bottom: 1px solid #222;
